4a271e7ff57fc106caf689ab3ad1a5eae8bd9b8a
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-11 15:38:17 +0430

    fethced data from API and saved into coins state

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+//API
+import { getCoin } from '../services/api';
 
 const Landing = () => {
+
+    const [coins, setCoins] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            const data = await getCoin();
+            setCoins(data);
+        }
+        fetchAPI();
+    }, [])
+
     return (
         <div>
             
