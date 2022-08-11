2a00985386e2e1e0a765c2bb26de1385a1f25ab8
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-11 16:54:30 +0430

    Loading.js styled

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import styles from "./Loading.module.css";
+
 //Gif
 import LoadingGif from "../Gif/loading-gif.gif";
 
 const Loading = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <img src={LoadingGif} alt='Loading'/>
             <h1>Loading...</h1> 
         </div>
